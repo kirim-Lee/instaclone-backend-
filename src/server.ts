334a9278e0ab128f873fb17bec0312d8c73d8a4e
Kirim-Lee <uthi1004@gmail.com>
2021-05-20 10:01:32 +0900

    add dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { ApolloServer } from 'apollo-server';
 import schema from './schema';
 
 const server = new ApolloServer(schema);
 
-server.listen().then(({ url }) => console.log(`server is running ${url}`));
+const PORT = process.env.PORT;
+
+server
+  .listen(PORT)
+  .then(({ url }) => console.log(`server is running ${url} ✅`));
