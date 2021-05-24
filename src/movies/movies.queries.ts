1bb8b7fa92be16b1087f71448eee65009b4e6cb3
Kirim-Lee <uthi1004@gmail.com>
2021-05-25 00:27:55 +0900

    add type definitions as users

--- src/movies/movies.queries.ts
+++
@@ -1,8 +0,0 @@
-import client from '../client';
-
-export default {
-  Query: {
-    movies: () => client.movie.findMany(),
-    movie: (_: any, { id }: any) => client.movie.findUnique({ where: { id } }),
-  },
-};
