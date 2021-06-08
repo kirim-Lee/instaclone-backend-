e84bfed008a5515e61ef6e1dd37a6fdd7ef4ec41
Kirim-Lee <uthi1004@gmail.com>
2021-06-08 09:42:00 +0900

    update profile

--- src/users/editProfile/editProfile.resolvers.ts
+++ src/users/editProfile/editProfile.resolvers.ts
@@ -1,5 +1,35 @@
+import client from '../../client';
+import bcrypt from 'bcrypt';
+interface IEditProfile {
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
 export default {
   Mutation: {
-    editProfile: () => console.log('hi'),
+    editProfile: async (_, edit: Partial<IEditProfile>) => {
+      if (edit.password) {
+        const uglyPassword = await bcrypt.hash(edit.password, 10);
+        edit.password = uglyPassword;
+      }
+      try {
+        const user = await client.user.update({ where: { id: 1 }, data: edit });
+        if (user.id) {
+          return {
+            ok: true,
+          };
+        } else {
+          return {
+            ok: false,
+            error: 'something is wrong so couln`t update',
+          };
+        }
+      } catch (e) {
+        return e;
+      }
+    },
   },
 };
